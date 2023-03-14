da31e54726f2f736cd0325fb696d82a55d0ac408
fcsk <kfocusek@gmail.com>
2023-03-14 20:34:21 +0100

    added rates to inputs

--- js/app.js
+++ js/app.js
@@ -2,6 +2,14 @@ const url = "https://api.nbp.pl/api/exchangerates/tables/a/?format=json";
 let PLN = {};
 let CurrencyRates;
 
+const fromCurrency = document.getElementById("fromCurrency");
+const toCurrency = document.getElementById("toCurrency");
+const inputAmount = document.getElementById("amount");
+const convertButton = document.getElementById("convert");
+
+// console.log(fromCurrency);
+// console.log(toCurrency);
+
 fetch(url)
   .then((res) => res.json())
   .then((data) => {
@@ -12,8 +20,17 @@ fetch(url)
     PLN["code"] = "PLN";
     PLN["mid"] = 1;
     CurrencyRates.push(PLN);
-    console.log(CurrencyRates);
 
-    CurrencyRates.forEach((rate) => {});
+    CurrencyRates.forEach((element) => {
+      const option = document.createElement("option");
+      option.text = `${element.currency} [${element.code}]`;
+      option.value = element.code;
+      fromCurrency.add(option);
+
+      const option2 = document.createElement("option");
+      option2.text = `${element.currency} [${element.code}]`;
+      option2.value = element.code;
+      toCurrency.add(option2);
+    });
   })
   .catch((error) => console.log(error));
