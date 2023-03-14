312c159bc44af6faffc3d5a09e6270fc411e1443
fcsk <kfocusek@gmail.com>
2023-03-14 20:19:01 +0100

    Added polish currency. Change conception in html

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 const url = "https://api.nbp.pl/api/exchangerates/tables/a/?format=json";
+let PLN = {};
+let CurrencyRates;
 
 fetch(url)
   .then((res) => res.json())
   .then((data) => {
-    data.forEach((currency) => {
-      currency.rates.forEach((element) => {
-        const names = `<li> ${element.currency}: ${element.mid.toFixed(
-          2
-        )}</li>`;
-        document.querySelector("ul")?.insertAdjacentHTML("beforeend", names);
-      });
-    });
+    CurrencyRates = data[0].rates;
+
+    /* ADD polish currency to json */
+    PLN["currency"] = "polski złoty";
+    PLN["code"] = "PLN";
+    PLN["mid"] = 1;
+    CurrencyRates.push(PLN);
+    console.log(CurrencyRates);
+
+    CurrencyRates.forEach((rate) => {});
   })
   .catch((error) => console.log(error));
